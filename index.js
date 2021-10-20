015834e431f76110051319e1e06490334a8f3e5a
Jake <xxjakexx14@gmail.com>
2021-10-19 20:35:46 -0400

    added validation to make sure users dont leave blank inputs

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const promptQuestionsP1 = () => {
     {
       type: "input",
       name: "title",
-      message: "What is the title of your project? (Required)",
+      message: "What is the title of your project?",
       validate: (titleInput) => {
         if (titleInput) {
           return true;
         } else {
-          console.log("Please enter your project title.");
+          console.log("\nPlease enter your project title.");
           return false;
         }
       },
@@ -21,16 +21,40 @@ const promptQuestionsP1 = () => {
       type: "input",
       name: "description",
       message: "Write a breif description about your project: ",
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("\nPlease enter a breif description about your project.");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "installationDesc",
       message: "Write a brief description for the installation process: ",
+      validate: (installDescInput) => {
+        if (installDescInput) {
+          return true;
+        } else {
+          console.log("\nPlease enter a breif description about your projects installation process.");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "usage",
       message: "What is the usage of this project?",
+      validate: (usageInput) => {
+        if (usageInput) {
+          return true;
+        } else {
+          console.log("\nPlease enter the usage of your project.");
+          return false;
+        }
+      },
     },
     {
       type: "list",
@@ -42,6 +66,14 @@ const promptQuestionsP1 = () => {
       type: "input",
       name: "contributing",
       message: "Who are the contributors of this project?(Use commas to separate the names)",
+      validate: (contributingInput) => {
+        if (contributingInput) {
+          return true;
+        } else {
+          console.log("\nPlease enter the contributors of your project.");
+          return false;
+        }
+      },
     },
     {
       type: "confirm",
@@ -65,16 +97,40 @@ const promptQuestionsP1 = () => {
       type: "input",
       name: "questions",
       message: "Write a brief description if users are having issues/questions",
+      validate: (questionsInput) => {
+        if (questionsInput) {
+          return true;
+        } else {
+          console.log("\nPlease enter a brief description if users are having issues/questions.");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "github",
       message: "Please enter your Github username",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("\nPlease enter a valid Github username.");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "email",
       message: "Please enter your Email",
+      validate: (emailInput) => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("\nPlease enter a valid Email.");
+          return false;
+        }
+      },
     },
   ]);
 };
